fcfd50677410a6c682794def56e8ab636f08a887
zhaobao1830 <x5219438>
2019-03-15 16:12:26 +0800

    使用vue-cli3做的练习项目

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,87 +1,58 @@
 <template>
   <div class="hello">
-    <scroll ref="scroll" class="hello-content" :data="helloList">
-      <div>
-        <div v-if="helloList.length" class="slider-wrapper">
-          <div class="slider-content">
-            <van-swipe :autoplay="3000" indicator-color="white" :height="150">
-              <van-swipe-item v-for="(item, index) in helloList" :key="index">
-                <div class="name">{{item.name}}</div>
-                <div class="age">{{item.age}}</div>
-              </van-swipe-item>
-            </van-swipe>
-          </div>
-        </div>
-        <div class="hello-bottom">
-          <div v-for="(item, index) in helloList" :key="index">{{item.name}}</div>
-        </div>
-      </div>
-    </scroll>
+    <v-child1>
+      <template v-slot="scope">
+        <v-child v-for = "(item, index) in list"
+                 :type="item.type"
+                 :key="index"
+                 @click.native.prevent.stop="buttonMethods(scope)">
+        </v-child>
+      </template>
+    </v-child1>
   </div>
 </template>
 
 <script>
-  import Scroll from 'base/scroll/scroll'
+  import VChild1 from './child1'
+  import VChild from './child'
+
   export default {
   name: 'HelloWorld',
   data () {
     return {
-      helloList: [
-        {
-          name: 'test',
-          age: '123'
-        },
-        {
-          name: 'test',
-          age: '1233'
-        },
-        {
-          name: 'test',
-          age: '1234'
-        },
-        {
-          name: 'test',
-          age: '1235'
-        },
-        {
-          name: 'test',
-          age: '1236'
-        }
-      ]
+      list: [{ name: '编辑', func: 'handleEdit', type: 'edit' }, { name: '删除', func: 'handleDelete', type: 'del' }]
+    }
+  },
+  created () {
+  },
+  methods: {
+    buttonMethods (index) {
+      console.log(index)
     }
   },
   components: {
-    Scroll
+    VChild,
+    VChild1
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" rel="stylesheet/stylus" scoped>
- .hello
-   position: fixed
-   width: 100%
-   top: 0
-   bottom: 0
-   .hello-content
-     height: 100%
-     overflow: hidden
-     .slider-wrapper
-       position: relative
-       width: 100%
-       height: 0
-       padding-top: 40%
-       overflow: hidden
-       .slider-content
-         position: absolute
-         left: 0
-         top: 0
-         width: 100%
-         height: 100%
-         .name
-           height: 50px
-         .age
-           height: 50px
-   .hello-bottom
-     height: 1800px
+  @keyframes bounce-in
+    0% {
+      transform: scale(0)
+    }
+    50% {
+      transform: scale(1.5)
+    }
+    100% {
+      transform: scale(1)
+    }
+  .active
+    transform-origin: left center
+    animation: bounce-in 1s
+  .leave
+    transform-origin: left center
+    animation: bounce-in 1s reverse
 </style>
