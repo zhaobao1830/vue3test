e7e3c0129a8767f13aee158ace2b45189a0c3c9e
zhaobao1830 <x5219438>
2019-03-19 09:04:09 +0800

    多个元素或组件的过渡；同一个标签切换的时候，要添加key

---
+++ src/components/duogedonghua.vue
@@ -0,0 +1,35 @@
+<template>
+  <!--多个元素或组件的过渡-->
+  <div class="root">
+    <transition mode="in-out">
+      <div v-if="show" key="hello">Hello World</div>
+      <div v-else key="bye">Bye World</div>
+    </transition>
+    <button @click="handleClick">toggle</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'duogedonghua',
+    data () {
+      return {
+        show: true
+      }
+    },
+    methods: {
+      handleClick () {
+        this.show = !this.show
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .v-enter, .v-leave-to {
+    opacity: 0;
+  }
+  .v-enter-active, .v-leave-active{
+    transition: opacity 1s;
+  }
+</style>
