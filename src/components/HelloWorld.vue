a303a054faf4d10e39889f6bab2bcf8360b458ef
zhaobao1830 <373098777@qq.com>
2019-04-09 23:36:48 +0800

    测试slot

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,87 +1,25 @@
 <template>
   <div class="hello">
-    <scroll ref="scroll" class="hello-content" :data="helloList">
-      <div>
-        <div v-if="helloList.length" class="slider-wrapper">
-          <div class="slider-content">
-            <van-swipe :autoplay="3000" indicator-color="white" :height="150">
-              <van-swipe-item v-for="(item, index) in helloList" :key="index">
-                <div class="name">{{item.name}}</div>
-                <div class="age">{{item.age}}</div>
-              </van-swipe-item>
-            </van-swipe>
-          </div>
-        </div>
-        <div class="hello-bottom">
-          <div v-for="(item, index) in helloList" :key="index">{{item.name}}</div>
-        </div>
-      </div>
-    </scroll>
+    <test-props>
+      <template v-slot:testSlot="slotProps">
+        <p>{{slotProps.msgs.msg}}</p>
+        <p>{{slotProps.tt}}</p>
+      </template>
+    </test-props>
   </div>
 </template>
 
 <script>
-  import Scroll from 'base/scroll/scroll'
+  import TestProps from 'components/TestProps'
+
   export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      helloList: [
-        {
-          name: 'test',
-          age: '123'
-        },
-        {
-          name: 'test',
-          age: '1233'
-        },
-        {
-          name: 'test',
-          age: '1234'
-        },
-        {
-          name: 'test',
-          age: '1235'
-        },
-        {
-          name: 'test',
-          age: '1236'
-        }
-      ]
+    name: 'HelloWorld',
+    components: {
+      TestProps
     }
-  },
-  components: {
-    Scroll
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" rel="stylesheet/stylus" scoped>
- .hello
-   position: fixed
-   width: 100%
-   top: 0
-   bottom: 0
-   .hello-content
-     height: 100%
-     overflow: hidden
-     .slider-wrapper
-       position: relative
-       width: 100%
-       height: 0
-       padding-top: 40%
-       overflow: hidden
-       .slider-content
-         position: absolute
-         left: 0
-         top: 0
-         width: 100%
-         height: 100%
-         .name
-           height: 50px
-         .age
-           height: 50px
-   .hello-bottom
-     height: 1800px
 </style>
---
+++ src/components/TestProps.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="testProps">
+    <main>
+      <slot name="testSlot" :msgs="msgs" :tt="tt">{{msgs.msg1}}</slot>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TestProps',
+    data () {
+      return {
+        msgs: {
+          msg: '123',
+          msg1: '345'
+        },
+        tt: '666'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
